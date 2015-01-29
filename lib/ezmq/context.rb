3ae2b4a662b046436863f5067619b31ce94f50be
Chris Olstrom <chris@olstrom.com>
2015-01-29 12:39:40 -0800

    Actually included ezmq/context in gem. Derp.

---
+++ lib/ezmq/context.rb
@@ -0,0 +1,22 @@
+require 'ffi-rzmq'
+
+# Syntactic sugar for 0MQ, because Ruby shouldn't feel like C.
+module EZMQ
+  # Wrapper class to simplify 0MQ sockets.
+  class Context < ZMQ::Context
+    # Creates a 0MQ context.
+    #
+    # Contexts are essentially resource containers or sandboxes for 0MQ. They
+    #   allow multiple sockets to share access to system resources, and an
+    #   entire context can be terminated, closing all sockets within it.
+    #
+    # Contexts are useful when dealing with the 'inproc' transport / protocol.
+    #   Any sockets that need to communicate in-process must share a context.
+    #
+    # @return [Context] a new instance of Context.
+    #
+    def initialize
+      super
+    end
+  end
+end
